c4fd3a8289183b83e2ece7cb08657ccc251538b3
Qandeel Kamran. <160300255+Qandeel-Kamran@users.noreply.github.com>
2025-04-28 01:07:06 +0500

    app.py

---
+++ homework_projects/00_intro_python/02_agreement_bot.md/app.py
@@ -0,0 +1,8 @@
+def main():
+    print("Let’s kick things off with a fun round of Q&A—just a quick way to spark some energy and get to know each other better!! :)")
+    q1: str=input("What's your favorite animal?")
+    print(f"That's great my favourite animal is also {q1}!")
+
+
+if __name__ == '__main__':
+    main()
